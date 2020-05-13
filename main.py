6559cdede98970a56fc9e3eaaf8f80ba83e9d676
x2sim <49098561+x2sim@users.noreply.github.com>
2020-05-13 11:31:09 +0300

    Update main.py
    fix for pep8, fix naming

--- main.py
+++ main.py
@@ -1,57 +1,58 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import sys
 import requests
 import os
 from dotenv import load_dotenv
-load_dotenv()
 import argparse
 
-TOKEN_FOR_BITLINK=os.getenv("BITLINK_TOKEN")
+TOKEN_FOR_BITLINK = os.getenv('BITLINK_TOKEN')
+
 
 def create_parser():
-    parser = argparse.ArgumentParser(
-        prog='bitlink',
-        description='''Программа делает ссылки короткими, либо указывает количество переходов по короткой ссылке''',
-        epilog='''(c) Александр Дубков 2020.'''
+    parser = argparse.ArgumentParser(prog='bitlink',
+        description = '''Программа делает ссылки короткими, либо указывает количество переходов по короткой ссылке''',
+        epilog = '''(c) Александр Дубков 2020.'''
     )
-    parser.add_argument('--link', '-l', help='Ссылка')
+    parser.add_argument('--link', '-l', help = 'Ссылка')
     return parser
 
 def shorten_link(token, link):
-    url='https://api-ssl.bitly.com/v4/shorten'
-    json={"long_url": link}
-    headers={"Authorization": "Bearer {}".format(token)}
-    response=requests.post(url,json=json,headers=headers)
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    json = {'long_url': link}
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+    response = requests.post(url, json=json, headers=headers)
     response.raise_for_status()
-    short_link=response.json()["id"]    
+    short_link = response.json()['id']
     return short_link
 
 def count_clicks(token, link):
-    url='https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(link)
-    headers={"Authorization": "Bearer {}".format(token)}
-    payload={'units':'-1'}
-    response = requests.get(url, headers=headers,params=payload)
-    response.raise_for_status()    
-    clicks_count=response.json()['total_clicks']    
+    url = \
+        'https://api-ssl.bitly.com/v4/bitlinks/{}/clicks/summary'.format(link)
+    headers = {'Authorization': 'Bearer {}'.format(token)}
+    payload = {'units': '-1'}
+    response = requests.get(url, headers=headers, params=payload)
+    response.raise_for_status()
+    clicks_count = response.json()['total_clicks']
     return clicks_count
 
 
 if __name__ == '__main__':
+    load_dotenv()
     parser = create_parser()
-    namespace = parser.parse_args(sys.argv[1:])
-    url=namespace.link
-    if url.startswith("bit.ly/"):
+    arguments_of_argparse = parser.parse_args(sys.argv[1:])
+    url = arguments_of_argparse.link
+    if url.startswith('bit.ly/'):
         try:
-            total_clicks = count_clicks(TOKEN_FOR_BITLINK,url)
+            total_clicks = count_clicks(TOKEN_FOR_BITLINK, url)
         except requests.exceptions.HTTPError:
             print("Ссылка не корректная")
         else:
             print("По вашей ссылке прошли {} раз(а)".format(total_clicks))
     else:
         try:
-            short_link = shorten_link(TOKEN_FOR_BITLINK,url)       
+            short_link = shorten_link(TOKEN_FOR_BITLINK, url)       
         except requests.exceptions.HTTPError:
             print("Ссылка не корректная")
         else:
             print("Короткая ссылка: {}".format(short_link))
-
-
